caa7fd3aad19bea7b65b884af433ab0a8f9eef47
Paphangkon <paphangkon009@gmail.com>
2026-04-18 23:32:32 +0700

    fix: proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,10 +1,10 @@
-import { withAuth } from "next-auth/middleware";
+import { NextRequest, NextResponse } from "next/server";
 
-export const proxy = withAuth({
-  callbacks: {
-    authorized: ({ token }) => !!token,
-  },
-});
+export function proxy(req:NextRequest) {
+  const token = req.cookies.get("token")?.value;
+  if(!token) return NextResponse.redirect(new URL('/login', req.url));
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: ["/api/reservations/:path*", "/reservations/:path*"],
